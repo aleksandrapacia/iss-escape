d379b635a0326f47d92491abe7d8a04b23858f03
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-04-23 23:21:43 +0200

    Player not going out off screen (left),

--- master.py
+++ master.py
@@ -3,7 +3,7 @@ import sys
 from station import ISS
 
 # Screen settings
-(width, height) = (730, 487)
+(width, height) = (600, 487)
 screen = pygame.display.set_mode((width, height))
 
 # Caption
@@ -15,7 +15,7 @@ background = pygame.image.load('bg.jpg')
 # Station
 iss_file = open('iss.png')
 texture_station = pygame.image.load(iss_file)
-iss = ISS(screen, 243.5, 380, texture_station)
+iss = ISS(screen, 200, 380, texture_station)
 
 # Main loop
 running = True
@@ -32,7 +32,11 @@ while running:
     elif all_keys[pygame.K_RIGHT]:
         iss.pos_x += 0.15
         print('right')
-    
+
+    # Keeping player on screen
+    if iss.pos_x < 0:
+        iss.pos_x = 0
+
     screen.blit(background, (0,0))
     screen.blit(iss.texture, (iss.pos_x, iss.pos_y)) 
     pygame.display.flip()
--- station.py
+++ station.py
@@ -1,7 +1,7 @@
 import pygame
 
 class ISS(pygame.sprite.Sprite):
-    def __init__(self, screen, pos_x: int, pos_y: float, texture: pygame.Surface):
+    def __init__(self, screen, pos_x: float, pos_y: float, texture: pygame.Surface):
         """ Reading an image and downloading rect """
         self.image = pygame.image.load('iss.png')
         self.screen = screen
@@ -9,6 +9,6 @@ class ISS(pygame.sprite.Sprite):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.texture = texture
-
+        
     def render(self):
         self.screen.blit(self.image, self.rect)
