eb57a658cc2974ec8755378d24bdc332242ae062
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-04-26 22:29:30 +0200

    ISS movement improvements

--- master.py
+++ master.py
@@ -5,6 +5,7 @@ from station import ISS
 # Screen settings
 (width, height) = (600, 487)
 screen = pygame.display.set_mode((width, height))
+screen_rect = screen.get_rect()
 
 # Caption
 pygame.display.set_caption('ISS Escape')
@@ -33,9 +34,12 @@ while running:
         iss.pos_x += 0.15
         print('right')
 
+
     # Keeping player on screen
     if iss.pos_x < 0:
         iss.pos_x = 0
+    if iss.pos_x > 420:
+        iss.pos_x = 420
 
     screen.blit(background, (0,0))
     screen.blit(iss.texture, (iss.pos_x, iss.pos_y)) 
