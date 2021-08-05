d22ecd773bcb1b5a086f7bc7806a2c4f2c7e6e3a
Aleksandra Pacia <olusiaOP05@gmail.com>
2021-08-05 22:18:53 +0200

    added valid comment about collision*
    *collision - between enemy and bullet

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ pygame.init()
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 487
 STATION_HEIGHT = 380
-BULLET_SPEED = 5
-ENEMY_SPEED = 1
+BULLET_SPEED = 4
+ENEMY_SPEED = 1.02
 
 # Screen settings
 (width, height) = (SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -37,14 +37,11 @@ enemy_texture = pygame.image.load(enemy_file)
 enemies: list[Enemy] = []
 
 # Bullets
+bulletX = 90
+bulletY = 390
 bullet_file = open("assets/textures/bullet.png")
 bullet_texture = pygame.image.load("assets/textures/bullet.png")
 bullets: list[Bullet] = []
-bulletX = 90
-bulletY = 390
-
-score = 0
-
 
 # Caption
 pygame.display.set_caption("ISS Escape")
@@ -57,36 +54,43 @@ shot_sound = pygame.mixer.Sound("assets/sounds/shot.wav")
 explosion_sound = pygame.mixer.Sound("assets/sounds/explosion.wav")
 
 score = 0
+
 # Main loop
 running = True
 while running:
     all_event = pygame.event.get()
     for event in all_event:
-        if event. type == pygame.QUIT:
+        if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            shot_sound.play()
-            # Bullets
+            # Bullets' class
             bullet = Bullet(
                 bulletX + station.pos_x,
                 STATION_HEIGHT + 10,
                 bullet_texture,
                 BULLET_SPEED,
-            )           
-
+            )
             bullets.append(bullet)
+            shot_sound.play()
+            # Checking whether the bullet hits the enemy
+             # hits = pygame.Rect.collidelist(enemies, bullets) < -- (?)
+             # if hits:
+                 #print('c')          
 
-    # Enemies moves
-    for enemy in enemies:
-        for i in range(3):
-            enemy.move()
-
-    for i in range(3):
-        enemy = Enemy(random.randrange(67, 520), -30, enemy_texture, ENEMY_SPEED)
+    # Multiply enemies
+    for i in range(4):
+        enemy = Enemy(random.randrange(67, 520), -20, enemy_texture, ENEMY_SPEED)
         start_time+=1
         if start_time > 200:
             enemies.append(enemy)
             start_time = 0
+
+    # Bullets' and enemies' movements
+    for bullet in bullets:
+        bullet.move() 
+    for enemy in enemies:
+        enemy.move()
+
     # Station moves
     all_keys = pygame.key.get_pressed()
     if all_keys[pygame.K_LEFT]:
@@ -94,18 +98,12 @@ while running:
     elif all_keys[pygame.K_RIGHT]:
         station.pos_x += 4
 
-    my, mx = pygame.mouse.get_pos()
-
-    for bullet in bullets:
-        bullet.move()
-
-    # Iterate over a slice copy if you want to mutate a list.
+    # Removing enemy if it goes off screen
     for bullet in bullets[:]:
         if bullet.pos_x < 0:
             bullets.remove(bullet)
-
-        for i in range(len(bullets)):
-            bullet = bullets[i]
+    for i in range(len(bullets)):
+        bullet = bullets[i]
 
     # Keeping player on screen
     if station.pos_x < 0:
@@ -114,12 +112,17 @@ while running:
         station.pos_x = 420
     screen.blit(background_texture, (0, 0))
 
+    # Displayin bullets
     for bullet in bullets:
         screen.blit(bullet_texture, pygame.Rect(bullet.pos_x, bullet.pos_y, 0, 0))
-
+    
+    # Displaying enemies' rectangles 
     for enemy in enemies:
         screen.blit(enemy.texture, pygame.Rect(enemy.pos_x, enemy.pos_y, 0, 0))
-        
+    
+    # Displaying stations' texture
     screen.blit(station.texture, (station.pos_x, station.pos_y))
+
     pygame.display.flip()
     clock.tick(60)
+    
